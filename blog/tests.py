19b0d45ad4809ee989258ff4cd3cd6c43f20de12
Alcibiades Bustillo <bustillo.alcibiades@gmail.com>
2020-08-20 12:44:45 -0400

    Create CRUD and test of post

--- blog/tests.py
+++ blog/tests.py
@@ -42,3 +42,35 @@ class BlogTest(TestCase):
         self.assertTemplateUsed(response, 'blog/post_detail.html')
         self.assertContains(response, 'Nice body content')
     
+
+    # New test
+    """
+        def test_get_absolute_url
+        def test_post_create_view
+        def test_post_update_view
+        def test_post_delete_view
+    """
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+    
+    def test_post_create_view(self): # new
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+
+    def test_post_update_view(self): # new
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self): # new
+        response = self.client.get(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)
